4c4131f117f840e9cad6e88f8e204762b7f7b294
Douglas Utzig <douglasutzig.1@gmai.com>
2024-05-03 10:09:51 -0300

    commitando trabalho

--- arquivo_teste.js
+++ arquivo_teste.js
@@ -1,1 +1,10 @@
-//arquivo para testar commit no git hub
+let n = Math.PI;
+let raio = 2.00;
+let area = n * (raio * raio);
+area = area.toFixed(4);
+
+console.log("A=" + area);
+
+/*
+Inserindo informações na branch funcionalidade2
+*/
--- desafio_area.js
+++ desafio_area.js
@@ -1,7 +1,6 @@
 const PI = Math.PI;
 let raio = 10;
 
-
 let area = PI * (raio * raio);
 
 console.log("A área do circuferencia é " + area + " M2");
